2d7085b06fbb2de083e141df7e27270db6ed46af
Tchetche Herman <106983897+devmobux@users.noreply.github.com>
2024-07-12 20:21:33 +0000

    u

---
+++ functions/api/write/items/[[path]].js
@@ -0,0 +1,17 @@
+import { S3Client, parseBucketPath } from "../../../../utils/s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+
+
+const s3Client = S3Client;
+
+
+async function onRequestPost(context) {
+    const key = parseBucketPath(context);
+
+    const bucket = process.env.R2_BUCKET_NAME;
+
+    const url = await getSignedUrl(s3Client, new PutObjectCommand({ Bucket: bucket, Key: key }));
+ 
+    return c.json({ key, url });
+}
---
+++ utils/s3.js
@@ -0,0 +1,18 @@
+import { S3Client } from "@aws-sdk/client-s3"
+
+export const S3Client = new S3Client({
+    region: 'auto',
+    endpoint: process.env.R2_ENDPOINT,
+    credentials: {
+      accessKeyId: process.env.R2_ACCESS_KEY_ID,
+      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    },
+})
+
+
+export function parseBucketPath(context) {
+  const pathSegments = (context.params.path || []);
+  const path = decodeURIComponent(pathSegments.join("/"));
+
+  return path;
+}
