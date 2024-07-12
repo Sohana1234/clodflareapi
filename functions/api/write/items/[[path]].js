c334de79f087756f58804b2e640bfed5fe819f21
Tchetche Herman <106983897+devmobux@users.noreply.github.com>
2024-07-12 21:00:44 +0000

    u

--- functions/api/write/items/[[path]].js
+++ functions/api/write/items/[[path]].js
@@ -1,9 +1,10 @@
-import { r2, parseBucketPath } from "../../../../utils/s3";
+import { r2Factory, parseBucketPath } from "../../../../utils/s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 
 export async function onRequestPost(context) {
+    const r2 = r2Factory(context.env);
     const key = parseBucketPath(context);
 
     const bucket = process.env.R2_BUCKET_NAME;
