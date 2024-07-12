98489c30d982c37efd96a91b84417143c9ed0434
Tchetche Herman <106983897+devmobux@users.noreply.github.com>
2024-07-12 20:59:41 +0000

    u

--- utils/s3.js
+++ utils/s3.js
@@ -1,14 +1,15 @@
 import { S3Client } from "@aws-sdk/client-s3"
 
-export const r2 = new S3Client({
+export function r2Factory(env) {
+  return new S3Client({
     region: 'auto',
-    endpoint: process.env.R2_ENDPOINT,
+    endpoint: env.R2_ENDPOINT,
     credentials: {
-      accessKeyId: process.env.R2_ACCESS_KEY_ID,
-      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+      accessKeyId: env.R2_ACCESS_KEY_ID,
+      secretAccessKey: env.R2_SECRET_ACCESS_KEY,
     },
-})
-
+  });
+}
 
 export function parseBucketPath(context) {
   const pathSegments = (context.params.path || []);
