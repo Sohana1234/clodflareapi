8b4065ee5999f00184d2cc9428c0b92603e0c3e8
Tchetche Herman <106983897+devmobux@users.noreply.github.com>
2024-07-12 21:08:20 +0000

    u

--- functions/api/write/items/[[path]].js
+++ functions/api/write/items/[[path]].js
@@ -6,10 +6,10 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 export async function onRequestPost(context) {
     const r2 = r2Factory(context.env);
     const key = parseBucketPath(context);
-
     const bucket = context.env.R2_BUCKET_NAME;
-
     const url = await getSignedUrl(r2, new PutObjectCommand({ Bucket: bucket, Key: key }));
- 
-    return c.json({ key, url });
+
+    return new Response(JSON.stringify({ key, url }), {
+        headers: { "Content-Type": "application/json" },
+    });
 }
